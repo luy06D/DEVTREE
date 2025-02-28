76e94aeb297149be9ffd575974031009d2768673
luy06D <cusiluis04@gmail.com>
2025-02-28 00:22:56 -0500

    hasheamos el password

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,21 +1,24 @@
 import {Request, Response} from 'express'
 import User from "../models/User"
+import { hashPassword } from '../utils/auth';
 
 // REQ ES LO QUE EL USUARIO ENVIA
 // RES LA RESPUESTA DEL SERVIDOR 
-export const createUsers = async (req: Request, res: Response) =>{
+export const createUsers = async (req: Request , res: Response )  =>{
 
-    const {email} = req.body;
+    const {email, password} = req.body;
 
-    const userExist = await User.findOne({email})
+    const userExist = await User.findOne({email}) 
     if(userExist){
         const error = new Error('El usuario ya esta registrado')    
-        return res.status(409).json({error: error.message})
+        res.status(409).json({error: error.message})
+        return;
     }
 
     const user = new User(req.body)
+    user.password = await hashPassword(password) //Hasheamos el password
 
     await user.save()
-    res.status(201).send('Registrado en existo')
+    res.status(201).send('Registrado con exito')
 
 }
