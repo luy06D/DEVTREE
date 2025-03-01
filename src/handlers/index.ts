51845e45ad87d919826d044dc5345f5ae3e79abb
luy06D <cusiluis04@gmail.com>
2025-03-01 00:39:29 -0500

    implementendo new entity handle

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express'
 import User from "../models/User"
 import { hashPassword } from '../utils/auth';
+import slug from 'slug'
 
 // REQ ES LO QUE EL USUARIO ENVIA
 // RES LA RESPUESTA DEL SERVIDOR 
@@ -15,8 +16,16 @@ export const createUsers = async (req: Request , res: Response )  =>{
         return;
     }
 
+    const handle = slug(req.body.handle, '');
+    const handleExist =  await User.findOne({handle})
+    if(handleExist){
+        const error = new Error('Nombre de usuario no disponible')
+        res.status(409).json({error: error.message})
+    }
+
     const user = new User(req.body)
     user.password = await hashPassword(password) //Hasheamos el password
+    user.handle = handle
 
     await user.save()
     res.status(201).send('Registrado con exito')
