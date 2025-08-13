77b729385ec248393397fa0858aace84df30d986
luy06D <cusiluis04@gmail.com>
2025-08-13 15:55:18 -0500

    agregando validacion con express-validator

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,11 +2,21 @@ import {Request, Response} from 'express'
 import User from "../models/User"
 import {hashPassword} from "../utils/auth"
 import slug from 'slug'
+import { validationResult } from 'express-validator'
 
 // USER REGISTER 
 export const createAccount = async (req: Request, res: Response) =>{
 
-    const {email, password} = req.body
+    // Manejar errores
+    let errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    
+    }
+
+    const {email, password} = req.body // Obtiene los datos del body
+
+
     const userExists = await User.findOne({email})
 
     if(userExists){
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,29 @@
 import { Router } from "express";
 import { createAccount } from "./handlers";
+import { body } from 'express-validator'
+
+
 const router = Router();
 
 // Routing -- CREAMOS LAS RUTAS DE LA APLICACIÓN
 
 /** Autenticación y Registro */
-router.post('/auth/register', createAccount);
+router.post('/auth/register',
+    body('handle')
+        .notEmpty()
+        .withMessage('El hanlde no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('El email no puede ir vacio'),
+    body('password')
+        .isLength({min: 8})
+        .notEmpty()
+        .withMessage('La contraseña debe tener como minimo 8 caracteres'),
+
+    createAccount);
 
 
 
